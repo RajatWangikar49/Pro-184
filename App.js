4df64e92bc03b523c3e9a20284e56f1ae95a39ff
RajatWangikar49 <84658365+RajatWangikar49@users.noreply.github.com>
2024-04-13 18:09:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+import Main from "./screens/Main";
+
+export default class App extends React.Component {
+
+  render() {
+    return(
+      <Main />
+    )
+  }
+
+}
